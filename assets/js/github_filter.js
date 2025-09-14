d8c56780d128780b1b77b2f128c2c3070944b7da
Chris <64163980+thenextbutton@users.noreply.github.com>
2025-09-14 22:34:11 +0100

    filter

--- assets/js/github_filter.js
+++ assets/js/github_filter.js
@@ -9,7 +9,30 @@ window.initGithubCategoryFilter = function () {
 
     items.forEach(item => {
       const category = item.getAttribute('data-category');
-      item.classList.toggle('hidden', !activeCategories.includes(category));
+      const shouldShow = activeCategories.includes(category);
+
+      if (shouldShow) {
+        item.classList.remove('collapsing', 'hidden');
+        item.style.display = 'block';
+
+        requestAnimationFrame(() => {
+          item.style.maxHeight = '1000px';
+          item.style.opacity = '1';
+        });
+      } else {
+        const clone = item.cloneNode(true);
+        item.parentNode.replaceChild(clone, item);
+        item = clone;
+
+        item.classList.add('collapsing');
+        item.style.maxHeight = '0';
+        item.style.opacity = '0';
+
+        item.addEventListener('transitionend', () => {
+          item.classList.add('hidden');
+          item.style.display = 'none';
+        }, { once: true });
+      }
     });
   }
 
